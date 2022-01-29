dac537b306718e33392eb13862af9462b36e5a94
kerryfrog <idasom060@gmail.com>
2022-01-29 23:32:38 +0900

    8.04

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -42,7 +42,32 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.width,
-            decoration: const,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end:Alignment.bottomCenter,
+                  colors: [
+                    Colors.white,
+                    Colors.white,
+                  ],
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(100),
+                bottomRight: Radius.circular(100),
+              )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/images/frog.png"
+                  )
+                )
+              ]
+
+            ),
           )
         ],
       )
